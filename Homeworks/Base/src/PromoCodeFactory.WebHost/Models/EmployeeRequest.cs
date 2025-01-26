c41916558d4430311b999e277908d1723b371542
ciasun <1_99q@mail.ru>
2025-01-27 00:25:18 +0300

    Знакомимся с ASP.NET Core // ДЗ

---
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Models/EmployeeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromoCodeFactory.WebHost.Models
+{
+    public class EmployeeRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<Guid> RoleIds { get; set; }
+    }
+}
