269ed5c953a4381cac36bef3853e5b79bddec9d0
ciasun <1_99q@mail.ru>
2025-03-08 16:14:07 +0300

    7 В качестве дополнительного задания реализовать методы PromoCodesController. Метод GivePromocodesToCustomersWithPreferenceAsync должен сохранять новый промокод в базе данных и находить клиентов с переданным предпочтением и добавлять им данный промокод. GetPromocodesAsync - здесь даты передаются строками, чтобы не было проблем с часовыми поясами

--- Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
@@ -1,39 +1,115 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.DataAccess.Repositories;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PromoCodeFactory.WebHost.Models;
 
 namespace PromoCodeFactory.WebHost.Controllers
 {
-    /// <summary>
-    /// Промокоды
-    /// </summary>
     [ApiController]
-    [Route("api/v1/[controller]")]
-    public class PromocodesController
-        : ControllerBase
+    [Route("api/[controller]")]
+    public class PromoCodesController : ControllerBase
     {
-        /// <summary>
-        /// Получить все промокоды
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
+        private readonly IRepository<PromoCode> _promoCodeRepository;
+        private readonly IRepository<Customer> _customerRepository;
+        private readonly IRepository<Preference> _preferenceRepository;
+
+        public PromoCodesController(
+            IRepository<PromoCode> promoCodeRepository,
+            IRepository<Customer> customerRepository,
+            IRepository<Preference> preferenceRepository)
         {
-            //TODO: Получить все промокоды 
-            throw new NotImplementedException();
+            _promoCodeRepository = promoCodeRepository ?? throw new ArgumentNullException(nameof(promoCodeRepository));
+            _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+            _preferenceRepository = preferenceRepository ?? throw new ArgumentNullException(nameof(preferenceRepository));
         }
 
         /// <summary>
-        /// Создать промокод и выдать его клиентам с указанным предпочтением
+        /// Создает новый промокод и добавляет его клиентам с указанным предпочтением.
         /// </summary>
-        /// <returns></returns>
+        /// <param name="preferenceName">Название предпочтения.</param>
+        /// <param name="promoCodeRequest">Данные для создания промокода.</param>
+        /// <returns>Список клиентов, которым был добавлен промокод.</returns>
         [HttpPost]
-        public Task<IActionResult> GivePromoCodesToCustomersWithPreferenceAsync(GivePromoCodeRequest request)
+        public async Task<IActionResult> GivePromocodesToCustomersWithPreferenceAsync(
+            [FromQuery] string preferenceName,
+            [FromBody] PromoCodeRequest promoCodeRequest)
         {
-            //TODO: Создать промокод и выдать его клиентам с указанным предпочтением
-            throw new NotImplementedException();
+            // Создаем новый промокод
+            var promoCode = new PromoCode
+            {
+                Id = Guid.NewGuid(),
+                Code = promoCodeRequest.Code,
+                BeginDate = DateTime.UtcNow, // Начало действия промокода
+                EndDate = DateTime.UtcNow.AddDays(promoCodeRequest.DurationInDays), // Конец действия промокода
+                PreferenceId = (await _preferenceRepository.GetAllAsync())
+                    .FirstOrDefault(p => p.Name == preferenceName)?.Id
+            };
+
+            if (promoCode.PreferenceId == null)
+            {
+                return NotFound($"Preference with name '{preferenceName}' not found.");
+            }
+
+            // Сохраняем промокод в базе данных
+            await _promoCodeRepository.AddAsync(promoCode);
+
+            // Находим клиентов с указанным предпочтением
+            var customersWithPreference = (await _customerRepository.GetAllAsync())
+                .Where(c => c.CustomerPreferences.Any(cp => cp.Preference.Name == preferenceName))
+                .ToList();
+
+            // Добавляем промокод каждому клиенту
+            foreach (var customer in customersWithPreference)
+            {
+                customer.PromoCodes.Add(promoCode);
+                await _customerRepository.UpdateAsync(customer);
+            }
+
+            return Ok(customersWithPreference.Select(c => new
+            {
+                c.Id,
+                c.FirstName,
+                c.LastName,
+                c.Email
+            }));
+        }
+
+        /// <summary>
+        /// Получает список промокодов в указанном диапазоне дат.
+        /// </summary>
+        /// <param name="beginDateStr">Дата начала в формате строки (например, "2023-10-01").</param>
+        /// <param name="endDateStr">Дата окончания в формате строки (например, "2023-10-31").</param>
+        /// <returns>Список промокодов.</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetPromocodesAsync(
+            [FromQuery] string beginDateStr,
+            [FromQuery] string endDateStr)
+        {
+            // Преобразуем строки дат в DateTime
+            if (!DateTime.TryParse(beginDateStr, out var beginDate) ||
+                !DateTime.TryParse(endDateStr, out var endDate))
+            {
+                return BadRequest("Invalid date format. Please use 'yyyy-MM-dd'.");
+            }
+
+            // Получаем промокоды из базы данных
+            var promoCodes = (await _promoCodeRepository.GetAllAsync())
+                .Where(p => p.BeginDate >= beginDate && p.EndDate <= endDate)
+                .Select(p => new
+                {
+                    p.Id,
+                    p.Code,
+                    BeginDate = p.BeginDate.ToString("yyyy-MM-dd"),
+                    EndDate = p.EndDate.ToString("yyyy-MM-dd"),
+                    PreferenceName = p.Preference?.Name
+                })
+                .ToList();
+
+            return Ok(promoCodes);
         }
     }
 }
